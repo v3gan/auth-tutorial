8428935b340f30aee6bb981696fa1437710e842c
marcus <marcmonk@gmail.com>
2022-11-04 08:27:20 -0400

    finished initial dev server setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login';
 
 function App() {
-  return <></>;
+  const [token, setToken] = useState();
+
+  if(!token){
+    return <Login setToken={setToken} />
+  }
+  
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preferences" element={<Preferences />} />
+        </Routes>
+      </BrowserRouter>
+    </div>);
 }
 
 export default App;
